d60a8128807d66c3f4e0dc2564b08e5949812388
Handrail9 <86343914+Handrail9@users.noreply.github.com>
2026-03-13 00:24:59 +0000

    iode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("plugin.serialization") version "2.2.21"
     kotlin("plugin.compose") version "2.2.21"
 }
+val iodeVersionCode: String by project
 
 android {
     compileSdk = 35
